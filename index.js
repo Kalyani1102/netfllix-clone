dd57e193ad20d059018b6c5a099d865e963c8c7f
Kalyani1102 <94071212+Kalyani1102@users.noreply.github.com>
2022-09-28 21:35:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import  ReactDOM  from  "react-dom";
+import './index.css';
+import  Card from "./Card";
+
+
+
+
+
+ReactDOM.render(
+  <>
+  <h1 className="heading_style"><b>List of top Netflix series in 2022</b></h1>
+   <Card imgsrc = "https://wallpaperaccess.com/full/1605503.jpg"
+   title =  " A Netflix Original Series"
+   sname = 'Dark'
+   link = 'https://www.netflix.com/in/'
+   />
+    <Card imgsrc = "https://c4.wallpaperflare.com/wallpaper/449/429/294/poster-stranger-things-netflix-wallpaper-preview.jpg"
+   title =  " A Netflix Original Series"
+   sname = 'STRANGER THINGS'
+   link = 'https://www.netflix.com/in/'
+   />
+    <Card imgsrc = "https://c.saavncdn.com/879/The-Fame-Game-Music-From-The-Netflix-Series--Hindi-2022-20220216040435-500x500.jpg"
+   title =  " A Netflix Original Series"
+   sname = 'THE FAME GAME'
+   link = 'https://www.netflix.com/in/'
+   />
+   <Card imgsrc = "https://media.gqindia.com/wp-content/uploads/2020/07/Flames.jpg"
+   title =  " A Netflix Original Series"
+   sname = 'FLAMES SEASON 2'
+   link = 'https://www.netflix.com/in/'
+   />
+    <Card imgsrc = "https://www.digitaltrends.com/wp-content/uploads/2021/10/squid-game-poster.jpg?p=1"
+   title =  " A Netflix Original Series"
+   sname = 'SQUID GAME'
+   link = 'https://www.netflix.com/in/'
+   />
+    <Card imgsrc = "https://m.media-amazon.com/images/M/MV5BNDJjMzc4NGYtZmFmNS00YWY3LThjMzQtYzJlNGFkZGRiOWI1XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg"
+   title =  " A Netflix Original Series"
+   sname = 'LUCIFER'
+   link = 'https://www.netflix.com/in/'
+   />
+    <Card imgsrc = "https://www.heavenofhorror.com/wp-content/uploads/2019/08/the-a-list-netflix-series-review.jpg"
+   title =  " A Netflix Original Series"
+   sname = 'THE ALIST'
+   link = 'https://www.netflix.com/in/'
+   />
+    <Card imgsrc = "https://www.thelondonmother.net/wp-content/uploads/2021/02/netflix_we_can_be_heroes-540x720.jpg"
+   title =  " A Netflix Original Series"
+   sname = 'WE CAN BE HEROES'
+   link = 'https://www.netflix.com/in/'
+   />
+    <Card imgsrc = "https://assets.gadgets360cdn.com/pricee/assets/product/202203/Mai_Netflix_poster_1648104279.jpeg"
+   title =  " A Netflix Original Series"
+   sname = 'MAI'
+   link = 'https://www.netflix.com/in/'
+   />
+    <Card imgsrc = "https://wirally.com/wp-content/uploads/2020/04/1-Top-Hindi-Web-Series-On-Netflix.jpg"
+   title =  " A Netflix Original Series"
+   sname = 'SCARED GAMES'
+   link = 'https://www.netflix.com/in/'
+   />
+    <Card imgsrc = "https://www.heavenofhorror.com/wp-content/uploads/2022/08/endless-night-netflix-review-1200x900.jpg"
+   title =  " A Netflix Original Series"
+   sname = 'ENDLESS NIGHT'
+   link = 'https://www.netflix.com/in/'
+   />
+    <Card imgsrc = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJXXVdDNys_fF5-5TuWQIFrZvwvFpPKo4kbptbQU9C3Rvb75Y1jkHxC2z1dxOY2sJAbbQ&usqp=CAU"
+   title =  " A Netflix Original Series"
+   sname = 'LEGICES'
+   link = 'https://www.netflix.com/in/'
+   />
+    <Card imgsrc = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTh4-g4NL6R-DaE0_bMUNGjDSZaRpWCprbsMQ&usqp=CAU"
+   title =  " A Netflix Original Series"
+   sname = 'KLEO'
+   link = 'https://www.netflix.com/in/'
+   />
+    {/* <Card imgsrc = "https://i.pinimg.com/750x/04/9f/74/049f74203c52ea7bc797fa337110e8e9.jpg"
+   title =  " A Netflix Original Series"
+   sname = 'JURASSIC WORLD CAMP CREATACEOUS'
+   link = 'https://collider.com/best-tv-shows-on-netflix-right-now/'
+   /> */}
+  </>,
+   document.getElementById('root')
+
+);
